d7855bcaa3ce50f4ee9fb3d6c012fe179b4a5876
Matias Ariel Coco <cocomatias6@gmail.com>
2023-12-29 11:27:05 -0300

    clase componentes finalizada
    Esto se va a seguir aplicando en las siguientes clases ya que esto es una introduccion

--- curso-angular-youtube/src/app/app.component.ts
+++ curso-angular-youtube/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'curso-angular-youtube';
+  title = 'Hola mundo en angular';
+  subtitle = 'Subtitulo';
 }
