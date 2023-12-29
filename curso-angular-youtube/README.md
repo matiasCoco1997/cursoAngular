677edb6934cdfdf95589a0eedf6fdc9904827d5e
Matias Ariel Coco <cocomatias6@gmail.com>
2023-12-29 10:52:05 -0300

    update creacion de proyecto angular
    Me rompia las rutas tuve que volver a crearlo

--- curso-angular-youtube/README.md
+++ curso-angular-youtube/README.md
@@ -1,4 +1,4 @@
-# 1CursoAngularYoutube
+# CursoAngularYoutube
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.8.
 
--- curso-angular-youtube/src/app/app.component.spec.ts
+++ curso-angular-youtube/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '1_curso-angular-youtube'`, () => {
+  it(`should have as title 'curso-angular-youtube'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('1_curso-angular-youtube');
+    expect(app.title).toEqual('curso-angular-youtube');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 1_curso-angular-youtube');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, curso-angular-youtube');
   });
 });
