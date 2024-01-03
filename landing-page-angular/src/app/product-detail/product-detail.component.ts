0f257f59ebd925fcea008d1e99cdbcd440461939
Matias Ariel Coco <cocomatias6@gmail.com>
2024-01-02 21:21:47 -0300

    update estructuras ng

--- landing-page-angular/src/app/product-detail/product-detail.component.ts
+++ landing-page-angular/src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product, productList } from '../products/products.mock';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,16 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit{
 
-  producto: string = '';
+  product?: Product;
+  productList: Product[] = productList;
+  loading: boolean = true;
   color: string = '';
 
   constructor (private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this._route.params.subscribe( params => {
-      this.producto = params['productId'];
-      this.color = params['category'];
-    })
+    setTimeout(() => {
+      this._route.params.subscribe( params => {
+        this.product = this.productList.find( product => product.id == params['productId']);
+        this.color = this.product?.price as number > 5 ? 'red' : ''
+        this.loading = false;
+      });
+    }, 1000)
   }
 
 }
